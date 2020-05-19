e7a56fa732acdebf63ac9c1e07e90df3cd7ee44f
Chelsea Wetzel <chelswetz@gmail.com>
2020-05-19 17:02:18 -0400

    solution done

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,1 +1,22 @@
 // code in here to make the check that a user has a token before giving them access to the cat facts
+
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config/secret.js');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if(token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if(err) {
+        // if token is not valid
+        res.status(401).json({ message: "sorry, no access" })
+      } else {
+        req.user = decodedToken.user;
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
+};
